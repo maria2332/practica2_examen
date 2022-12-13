00dcaf856b8adf4642ba96f9d2503f33cc564876
maria2332 <maria.ballesteros2004@gmail.com>
2022-12-13 10:40:30 +0100

    cambio

---
+++ ejercicio1.py
@@ -0,0 +1,11 @@
+"""
+Nota:
+• Los signos de libra por sí solos no cuentan, por ejemplo: la cadena "#" devolvería una matriz vacía.
+• Si una palabra está precedida por más de un hashtag, solo cuenta el último hashtag
+(por ejemplo, "##alot" devolvería ["alot"])
+• Los hashtags no pueden estar en medio de una palabra (por ejemplo, "hashtag en #línea" devuelve una matriz vacía)
+• Los hashtags deben preceder a los caracteres alfabéticos (por ejemplo, "#120398" o "#?" no son válidos)
+Entrada: cadena de palabras, donde algunas palabras pueden contener un hashtag.
+Salida: matriz de cadenas que tenían el prefijo del hashtag, pero que no contienen el hashtag.
+"""
+
